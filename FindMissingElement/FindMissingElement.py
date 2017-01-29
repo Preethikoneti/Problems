9f5b4aa7e5aca5e9737dd24b293d4b71aafc2a4a
nsk160130 <niks.ce15@gmail.com>
2017-01-29 16:39:24 -0600

    Update FindMissingElement.py

--- FindMissingElement/FindMissingElement.py
+++ FindMissingElement/FindMissingElement.py
@@ -10,9 +10,12 @@ def finder(arr1,arr2):
     # To check when the mismatch starts
     for i in range(min(len(arr1),len(arr2))):
         ot.append(arr1[i] == arr2[i])
-    
+        
     # Print the first mismatched value in arr1, since both are sorted array
     for i in ot:
         if i == False:
             print arr1[ot.index(i)]
+            break
+        else:       # If the mismatched element is in the last, return last element of arr1
+            print arr1[-1]
             break
