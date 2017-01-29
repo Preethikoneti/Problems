6ff7163955b8a31ca3a47193758c6143ab5cef78
nsk160130 <niks.ce15@gmail.com>
2017-01-29 16:32:06 -0600

    Update FindMissingElement.py

--- FindMissingElement/FindMissingElement.py
+++ FindMissingElement/FindMissingElement.py
@@ -1,1 +1,18 @@
-
+def finder(arr1,arr2):
+    
+    # Sort both the arrays
+    arr1.sort()
+    arr2.sort()
+    
+    # Create an empty list
+    ot = []
+    
+    # To check when the mismatch starts
+    for i in range(min(len(arr1),len(arr2))):
+        ot.append(arr1[i] == arr2[i])
+    
+    # Print the first mismatched value in arr1, since both are sorted array
+    for i in ot:
+        if i == False:
+            print arr1[ot.index(i)]
+            break
