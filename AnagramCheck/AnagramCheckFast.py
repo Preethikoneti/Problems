944dbaada8fc5dff98cfbdb1e04e7fdf917ccab6
nsk160130 <niks.ce15@gmail.com>
2017-01-29 15:04:21 -0600

    Update AnagramCheckFast.py

--- AnagramCheck/AnagramCheckFast.py
+++ AnagramCheck/AnagramCheckFast.py
@@ -1,1 +1,10 @@
+# This checks for Anagram without using dictionary approach.
+# First convert to lower case(lower(), then remove spaces(replace(" ","") and then punctuations(translate(None, string.punctuations).
 
+import string
+
+def anagram(s1,s2):
+    s1 = s1.lower().replace(" ","").translate(None, string.punctuation)
+    s2 = s2.lower().replace(" ","").translate(None, string.punctuation)
+    
+    return sorted(s1) == sorted(s2)
