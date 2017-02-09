3b479a2d85b5f6defa90f5015de3af830ef5ee95
nsk160130 <niks.ce15@gmail.com>
2017-02-08 22:13:53 -0600

    Update FindMissingElementFromRangeTotal.py

--- FindMissingElementFromRange/FindMissingElementFromRangeTotal.py
+++ FindMissingElementFromRange/FindMissingElementFromRangeTotal.py
@@ -1,1 +1,15 @@
-
+def MissingInteger_Total(lst,Range):
+    """
+    This method takes an input list of n-1 integers and given range of 1 to n and prints out the missing integer.
+    """
+    
+    n = len(Range)
+    
+    # Compute the total sum of n integers
+    total = (n*(n+1))/2
+    
+    # Subtract elements of input list from total, reslutant total is the missing integer
+    for i in lst:
+        total -= i
+    
+    return int(total)
