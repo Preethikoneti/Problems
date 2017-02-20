82201693c6675fb4fd57acb3dcb77b571ab35d36
nsk160130 <niks.ce15@gmail.com>
2017-02-19 18:50:29 -0600

    Update FindMissingElementFromRangeXOR.py

--- FindMissingElementFromRange/FindMissingElementFromRangeXOR.py
+++ FindMissingElementFromRange/FindMissingElementFromRangeXOR.py
@@ -1,6 +1,6 @@
 def MissingInteger_XOR(lst,Range):
     """
-    This method takes an input list of n-1 integers and given range of 1 to n and prints out the missing integer.
+    This method takes an input list of n-1 integers from given range of 1 to n integers and prints out the missing integer.
     """
     
     # Perform the XOR between two lists after converting them into sets, this will leave only missing integer,
